8e047f57abc82b4e35a4ba142352aea1f6536deb
itsnonostudio <itsnonostudio@gmail.com>
2026-09-23 15:25:55 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+const CONFIG = {
+  LIFF_ID: "2011708024-yFgNSgb6",
+  GAS_ENDPOINT: "https://script.google.com/macros/s/AKfycbzIwRV13bpePfAA32iJ8wloeFLoqaLja2aLlI84gKw4XESYr0CASobBde-KjjkFlSPH/exec",
+};
